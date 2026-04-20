5c45d912b95c609e94e65ad853d34071843ffc65
River Dewberry <riverdewberry@gmail.com>
2026-04-20 17:06:36 -0400

    add zig init

---
+++ src/main.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const Io = std.Io;
+
+const smoothShadows2D = @import("smoothShadows2D");
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    _ = io;
+}
