8060658e4275afcdbd4d074646508661e2f8bcff
River Dewberry <riverdewberry@gmail.com>
2026-04-21 20:23:32 -0400

    add raylib

--- build.zig
+++ build.zig
@@ -22,11 +22,21 @@ pub fn build(b: *std.Build) void {
             },
         }),
     });
+    
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
     const run_step = b.step("run", "Run the app");
-
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
 
--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,20 @@ const std = @import("std");
 const Io = std.Io;
 
 const smoothShadows2D = @import("smoothShadows2D");
+const rl = @import("raylib");
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
+    
+    rl.initWindow(500, 500, "Smooth shadows 2d");
+    defer rl.closeWindow();
 
-    _ = io;
+    while (!rl.windowShouldClose())
+    {
+
+        rl.beginDrawing();
+
+        rl.endDrawing();
+    }
+
+    _ = init;
 }
