74f4cb380674aa76e13e07fc37d0207c238e6e38
River Dewberry <riverdewberry@gmail.com>
2026-04-22 21:10:50 -0400

    add texture to draw

--- src/main.zig
+++ src/main.zig
@@ -9,10 +9,23 @@ pub fn main(init: std.process.Init) !void {
     rl.initWindow(500, 500, "Smooth shadows 2d");
     defer rl.closeWindow();
 
+    const baseTexture = try rl.loadTexture("../textures/white.png");
+    defer rl.unloadTexture(baseTexture);
+
+    rl.setTargetFPS(30);
+
     while (!rl.windowShouldClose())
     {
 
         rl.beginDrawing();
+
+        baseTexture.drawPro(
+            rl.Rectangle.init(0.0, 0.0, 1.0, 1.0),
+            rl.Rectangle.init(0.0, 0.0, 500.0, 500.0),
+            rl.Vector2.init(0.0, 0.0),
+            0,
+            rl.Color.white
+        );
 
         rl.endDrawing();
     }
